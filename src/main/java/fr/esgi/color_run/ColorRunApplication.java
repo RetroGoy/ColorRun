ff4f4284758949873fe88024b2683cc93106686d
Nicolas1904 <nicolas.ryckelynck@epitech.eu>
2025-06-11 10:01:27 +0200

    [UPDATE] - style page

--- src/main/java/fr/esgi/color_run/ColorRunApplication.java
+++ src/main/java/fr/esgi/color_run/ColorRunApplication.java
@@ -2,16 +2,10 @@ package fr.esgi.color_run;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class ColorRunApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(ColorRunApplication.class);
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(ColorRunApplication.class, args);
--- src/main/java/fr/esgi/color_run/config/SecurityConfig.java
+++ src/main/java/fr/esgi/color_run/config/SecurityConfig.java
@@ -3,22 +3,23 @@ package fr.esgi.color_run.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
                 http
+                                .csrf(AbstractHttpConfigurer::disable)
                                 .authorizeHttpRequests(auth -> auth
                                                 .requestMatchers("/", "/home", "/register", "/login", "/contact",
-                                                                "/about",
-                                                                "/races", "/races/*", "/resources/**", "/webjars/**",
+                                                                "/about", "/races", "/races/*", "/resources/**",
+                                                                "/webjars/**",
                                                                 "/css/**", "/js/**", "/images/**")
                                                 .permitAll()
                                                 .anyRequest().authenticated())
